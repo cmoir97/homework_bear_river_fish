af0c9fd29994fa03e6b2b5775215336f5e4d4146
Cameron Moir <cameronjmoir@gmail.com>
2019-09-17 17:06:40 +0100

    when bear catches fish fish is also removed from river

--- specs/river_spec.rb
+++ specs/river_spec.rb
@@ -9,6 +9,7 @@ class RiverTest < MiniTest::Test
   def setup()
     @river = River.new("Dee")
     @fish1 = Fish.new("Daniel")
+    @bear1 = Bear.new("Eli", "Brown")
 
   end
 
@@ -30,5 +31,20 @@ def test_remove_fish_from_river()
   @river.remove_fish_from_river(@fish1)
   assert_equal(0, @river.river_population_count())
 end
+
+def test_bear_takes_fish_from_river()
+  @river.add_fish_to_river(@fish1)
+  @bear1.bear_catches_fish(@fish1)
+  @river.remove_fish_from_river(@fish1)
+  assert_equal(0, @river.river_population_count())
+  assert_equal(1, @bear1.food_count())
+
+end
+
+
+
+
+
+
 
 end
